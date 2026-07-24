6b0c583acacdf00b8b8a7a2568d9397e156fbd25
Abdouroihamane Said <Abdoiroihamanesaid2023@gmail.com>
2026-07-24 23:07:12 +0530

    Encapsulatin and Bubble Sort

---
+++ Encapsulation.java
@@ -0,0 +1,20 @@
+class Student{
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String args[]){
+        Student s = new Student();
+
+        s.setName("Abdour");
+        System.out.println(s.getName());
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+## Java Day 9
+This repository contains my Java practice programs for Day 9.
+
+## Programs
+- Bubble Sort program
+- Encapsulation program
+
+# Ecapsulation
+Encapsulation means keeping data private and providing controlled access using methods.  
+`private` data cannot be accessed directly.  
+`public` methods (`set` and `get`) -> to modify and read the data.
